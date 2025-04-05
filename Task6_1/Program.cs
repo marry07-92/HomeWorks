4922fb794a894b832b2afb555d5a993eb1f40388
Marry Vasileva <vasilevamariia92@yandex.ru>
2025-04-05 14:15:41 +0400

    fix

---
+++ Task6_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /*
+           Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+            Знаки препинания не используются. Найти самое длинное слово в строке.
+          */
+
+
+        Console.Write("Введите предложение: ");
+
+            string vvod = Console.ReadLine(); //часть для ввода текста
+
+            while (vvod.Contains("  "))         //для того, чтобы в случае ввода с лишними пробелами, очистить для уменьшения времени по перебору
+            {
+                vvod = vvod.Replace("  ", " ");
+            }
+
+    vvod = vvod.Trim();
+
+            string[] words = vvod.Split(); // разделяю предложение на слова
+
+
+
+    int maxW = 0, index = 0;
+
+            for (int i = 0; i<words.Length; i++)
+            {
+                if (words[i].Length >= maxW)  
+                {
+                    maxW = words[i].Length;
+
+                    index = i;
+                }
+            }
+
+
+
+            Console.Write("Самое длинное слово: {0}", words[index]);
+
+
+Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ Task6_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+               Знаки препинания не используются. Составить программу, определяющую, является ли строка 
+                палиндромом без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).
+              */
+
+            string revs = "";
+
+            Console.WriteLine(" Введите предложение");
+
+            string vvod = Console.ReadLine(); //часть для ввода текста
+
+            string a = vvod.ToLower(); // привожу к нижнему регистру
+
+            string words = a.Replace(" ", ""); // убираю пробелы
+
+            Console.WriteLine("{0}", words);  // для проверки
+
+            for (int i = words.Length - 1; i >= 0; i--) //реверс
+            {
+                revs += words[i].ToString();
+            }
+            if (revs == words) // проверка предложение является палиндромом или нет
+            {
+                Console.WriteLine("Предложение является палиндром \n Введенное предложение {0} и перевернутое предложение {1}", words, revs);
+            }
+            else
+            {
+                Console.WriteLine("Предложение не являеться палиндром \n Введенное предложение {0} и перевернутое предложение {1}", words, revs);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
